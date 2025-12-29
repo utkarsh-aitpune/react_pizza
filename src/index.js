85566d2b1c392c7a92b644c9e2e61d6a1644fc51
utkarsh-aitpune <utkarsh102002@gmail.com>
2025-12-29 19:45:52 +0530

    building

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -8,3 +7,16 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+function App() {
+  return (
+    <div>
+      <h1>hello from pizza</h1>;
+      <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return <h1>hello from pizza</h1>;
+}
